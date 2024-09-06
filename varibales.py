b1e02d809e9a8cc0a1d101d6789eb35a490aadff
DAWOOD <dawood@feedni.net>
2024-09-06 11:55:57 +0500

    number lists practice done

--- varibales.py
+++ varibales.py
@@ -3,3 +3,19 @@ width=5.5
 area=length*width
 print(area*500)
 print(format(17,"b"))
+
+# n1=input("plz enter first number")
+# n2=input("plz enter second number")
+# print(n1+n2)
+mithai=["havla","eng","urdu"]
+print(mithai[0:0])
+print(len(mithai))
+print("havla" in mithai)
+print("havla" not in mithai)
+print("havla" not in mithai)
+mithai.append("apple")
+mithai.insert(2,"apple1")
+print(mithai)
+mithai=mithai+["dWOOSD"]
+mithai[0:2]="SAMOSA"
+print(mithai)
